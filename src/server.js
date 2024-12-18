c806cf8873e1948c91a2fd9a486a69d7b76f249a
VietYoRHa <thaiviet04@gmail.com>
2024-12-18 17:47:58 +0700

    node: add package cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from './config/connectDB';
+import cors from 'cors';
 
 require('dotenv').config();
 
 let app = express();
 
 // Config app
+app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
