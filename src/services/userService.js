3a71a48fcaa14d7e2fb85c1d93ccb9e45ef041cd
VietYoRHa <thaiviet04@gmail.com>
2025-05-02 15:53:12 +0700

    fix: change gender type to string

--- src/services/userService.js
+++ src/services/userService.js
@@ -128,8 +128,10 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === 1 ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId,
+                    image: data.avatar,
                 });
                 resolve({
                     errCode: 0,
